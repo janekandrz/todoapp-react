c102f440730e83dedbc0941a16a8de39d6dc8a5a
janekandrz <janekandrz@gmail.com>
2024-09-01 13:25:57 +0200

    new date

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,12 +1,16 @@
 function Footer() {
-    return (
-        <footer>
-            <hr></hr>
-            <p>
-                &copy; {new Date().getFullYear()}
-            </p>
-        </footer>
-    )
+  const day = String(new Date().getDay() + 1).padStart(2, "0");
+  const month = String(new Date().getMonth() + 1).padStart(2, "0");
+  const year = String(new Date().getFullYear());
+
+  return (
+    <footer>
+      <hr></hr>
+      <p>
+        {day} {month} {year}
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
